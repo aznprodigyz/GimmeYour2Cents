d0f3817ceb48bbb3bb52609a12793454453d19d1
binpeng zhang <binpeng.d.zhang@gmail.com>
2019-02-25 22:48:35 -0500

    added home page

--- server/server.js
+++ server/server.js
@@ -1,7 +1,4 @@
 var express = require('express');
-const hbs = require('hbs');
-const fs = require('fs');
-
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
 
@@ -9,39 +6,25 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+const hbs = require('hbs');
+const fs = require('fs');
+
 var app = express();
 const port = process.env.PORT || 3000;
+
 hbs.registerPartials(__dirname + "/views/partials");
 //set the view engine to hbs
 app.set('view engine', 'hbs');
-app.use((req, res, next) => {
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
-
-  console.log(log);
-  fs.appendFile('server.log', log + '\n', (err) => {
-    if (err) {
-      console.log("unable to append to server.log");
-    }
-  });
-  next()
-});
-app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear();
-});
+app.use(bodyParser.json());
 
-hbs.registerHelper('screamIt', (text) => {
-  return text.toUpperCase();
-});
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
   res.render('home.hbs', {
-    pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website'
+
   })
 });
+
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
