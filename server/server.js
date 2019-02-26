ea6c264fe21b7e7fd81e5b0e70aab7425b44352a
binpeng zhang <binpeng.d.zhang@gmail.com>
2019-02-25 22:31:38 -0500

    added directories

--- server/server.js
+++ server/server.js
@@ -8,10 +8,30 @@ var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
+hbs.registerPartials(__dirname + "/views/partials");
+//set the view engine to hbs
+app.set('view engine', 'hbs');
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
 
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log("unable to append to server.log");
+    }
+  });
+  next()
+});
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
 
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
   res.render('home.hbs', {
